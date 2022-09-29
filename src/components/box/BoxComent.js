a933bf63b0692bf7fe59325c2d410ed9ef1a6482
francodealesandro <Francodealesandro@gmail.com>
2022-09-29 18:22:42 -0300

    lint

--- src/components/box/BoxComent.js
+++ src/components/box/BoxComent.js
@@ -1,29 +1,37 @@
-import { Box, Button, ButtonGroup, Flex, IconButton, Center} from '@chakra-ui/react';
-import { MdBlock, MdBuild,MdDone} from "react-icons/md";
-import { IconName } from "react-icons/md";
-import {Link, useLoaderData} from "react-router-dom";
-import React, {useState} from "react";
+import { Box, Button, ButtonGroup } from "@chakra-ui/react";
+import { MdBlock, MdDone } from "react-icons/md";
+import React, { useState } from "react";
 
-export default function BoxComentarios({id, comentario}) {
-    const clase = useLoaderData();
-    const [visible, setVisible] = useState(false);
-    return (
-        <Box class="caja" boxShadow='dark-lg' backgroundColor="gray.100" m={3} borderRadius="10" borderWidth='3px' overflow='hidden'>
-            <Box class="texto">
-                <h1>{comentario}</h1>
-            </Box>
-            <Box class="abajo">
-                <ButtonGroup>
-                    <Button leftIcon={<MdDone />} colorScheme='red' variant='solid'>
-                        Aceptar
-                    </Button>
-                    <Button
-                     leftIcon={<MdBlock />} colorScheme='red' variant='solid' onClick={() => setVisible(!visible)}>
-                        {visible ? "Bloquear" : "Desbloquear"}
-                    </Button>
-                </ButtonGroup>
-            </Box>
-        </Box>
-      
-    )
-  }
+export default function BoxComentarios({ id, comentario }) {
+  const [visible, setVisible] = useState(false);
+  return (
+    <Box
+      class="caja"
+      boxShadow="dark-lg"
+      backgroundColor="gray.100"
+      m={3}
+      borderRadius="10"
+      borderWidth="3px"
+      overflow="hidden"
+    >
+      <Box class="texto">
+        <h1>{comentario}</h1>
+      </Box>
+      <Box class="abajo">
+        <ButtonGroup>
+          <Button leftIcon={<MdDone />} colorScheme="red" variant="solid">
+            Aceptar
+          </Button>
+          <Button
+            leftIcon={<MdBlock />}
+            colorScheme="red"
+            variant="solid"
+            onClick={() => setVisible(!visible)}
+          >
+            {visible ? "Bloquear" : "Desbloquear"}
+          </Button>
+        </ButtonGroup>
+      </Box>
+    </Box>
+  );
+}
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,13 +1,10 @@
-import React, {useContext} from "react";
 import Title from "../title/Title";
-import {ClaseContext} from "../contexts/ClasesContext";
-import {useNavigate} from "react-router-dom";
 import "./Header.css";
 
 export default function Header() {
-    const {clase, setClase} = useContext(ClaseContext);
-    return (<header className="header">
-            <Title name={"en el header esta esto"}/>
-        </header>
-    );
+  return (
+    <header className="header">
+      <Title name={"en el header esta esto"} />
+    </header>
+  );
 }
