14ce669ece16fbbc94f59e937241f06d23f9fd0c
LautyBona <lautybona@gmail.com>
2022-09-30 16:27:30 -0300

    dd

--- src/pages/CrearClase.js
+++ src/pages/CrearClase.js
@@ -1,31 +1,54 @@
-import { SimpleGrid, Button, Box, Grid} from '@chakra-ui/react';
-import {Link} from "react-router-dom";
-import {
-    Editable,
-    EditableInput,
-    EditableTextarea,
-    EditablePreview,
-  } from '@chakra-ui/react'
+import React, { Component } from "react";
+import { Input, ChakraProvider, Textarea } from "@chakra-ui/react";
+import "../pages/signin/SigninPage.css";
 
-export default function CajaCrearClases() {
-    return(
-        <div className="App">
-            <h1>Crear clases</h1>
-            <Box>
-                <Button m={3}>
-                        <Link to={"/clases"}>Volver</Link>  
-                </Button>
-                <Box class="caja"  backgroundColor="gray.100" m={3} borderRadius="10" borderWidth='3px' overflow='hidden'>
-                    <Box class="abajo">
-                        <Button>
-                            <Editable defaultValue='Take some chakra'>
-                                <EditablePreview />
-                                <EditableInput />
-                            </Editable>
-                        </Button>
-                    </Box>
-                </Box>             
-            </Box>
+export default class SigninTeacher extends Component {
+  render() {
+    return (
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <form>
+            <h3>Crear Clase</h3>
+            <div className="mb-3">
+              <label>Nombre de la materia</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Ingrese el nombre"
+              />
+            </div>
+            <div className="mb-3">
+              <label>Duración</label>
+              <input
+                type="duracion"
+                className="form-control"
+                placeholder="Ingrese la duracion"
+              />
+            </div>
+            <div className="mb-3">
+              <label>Frecuencia</label>
+              <input
+                type="frecuencia"
+                className="form-control"
+                placeholder="Ingrese la frecuencia"
+              />
+            </div>
+            <div className="mb-3">
+              <label>Costo</label>
+              <input
+                type="costo"
+                className="form-control"
+                placeholder="Ingrese el costo"
+              />
+            </div>
+            <div className="d-grid">
+              <button type="submit" className="btn btn-primary">
+                Crear clase
+              </button>
+            </div>
+          </form>
         </div>
-    )
+      </div>
+    );
+  }
 }
