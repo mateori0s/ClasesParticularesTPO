4272e88a313c5fb519dc44636cc8fb6028eef084
LautyBona <lautybona@gmail.com>
2022-11-25 18:05:54 -0300

    Update index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.send('Welcome to Express');
+  res.send("Esta es la pagina principal");
 });
 
 module.exports = router;
